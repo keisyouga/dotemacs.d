b7ba4f9c536549243c91c388d67226a23504781d
keisyouga <keisyouga@users.noreply.github.com>
2019-04-13 15:14:28 +0900

    custom setting to ~/.emacs.d/my-custom.el

--- init.el
+++ init.el
@@ -99,8 +99,8 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; custom
-;;(setq custom-file (concat (getenv "HOME") "/.emacs.d/my-custom.el"))
-;;(load custom-file)
+(setq custom-file (concat (getenv "HOME") "/.emacs.d/my-custom.el"))
+(load custom-file)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; package
@@ -185,16 +185,3 @@
 (put 'dired-find-alternate-file 'disabled nil)
 (put 'scroll-left 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil-numbers markdown-mode undo-tree evil))))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )

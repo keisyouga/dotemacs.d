904b69a23d80ef8890569270e14e9f0ba9787006
keisyouga <keisyouga@users.noreply.github.com>
2020-01-09 11:02:12 +0900

    disable C-\ key binding

--- init.el
+++ init.el
@@ -30,6 +30,7 @@
 (global-set-key (kbd "<C-S-tab>") 'previous-buffer)
 (global-set-key (kbd "C-C c") 'comment-region)
 (global-set-key (kbd "C-z") (lambda () (interactive) (message "instead of C-z, use C-x C-z to suspend")))
+(global-unset-key (kbd "C-\\"))         ; toggle-input-method
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; goto scratch
